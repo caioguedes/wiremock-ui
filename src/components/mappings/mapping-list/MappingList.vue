692b327776634fe3b58a18d5e766113bf2efaea1
Caio Guedes <caio.cesar.g.souza@gmail.com>
2021-02-10 19:29:03 -0300

    Create Mapping List Header

--- src/components/mappings/mapping-list/MappingList.vue
+++ src/components/mappings/mapping-list/MappingList.vue
@@ -1,15 +1,19 @@
 <template lang="pug">
-  ul#mapping-list.has-background-white-bis.side-shadow
-    Mapping(v-for="mapping in mappings" :mapping="mapping" :key="mapping.id")
+  div#mapping-list-wrap
+    MappingListHeader
+    ul#mapping-list.has-background-white-bis.side-shadow
+      Mapping(v-for="mapping in mappings" :mapping="mapping" :key="mapping.id")
 </template>
 
 <script>
 import Mapping from './Mapping'
+import MappingListHeader from './MappingListHeader'
 
 export default {
   name: 'MappingList',
   components: {
-    Mapping
+    Mapping,
+    MappingListHeader
   },
   props: ['mappings']
 }
@@ -17,10 +21,13 @@ export default {
 
 <style scoped>
   #mapping-list {
+    overflow-y: auto;
+    max-height: calc(100vh - 135px);
+  }
+
+  #mapping-list-wrap {
     z-index: 20;
     min-width: 25rem;
     max-width: 25rem;
-    height: 100%;
-    overflow-y: auto;
   }
 </style>
---
+++ src/components/mappings/mapping-list/MappingListHeader.vue
@@ -0,0 +1,24 @@
+<template lang="pug">
+  footer.is-clearfix.has-background-white-bis.bottom-shadow
+    .is-pulled-right
+      a.button.is-success(:disabled="!$store.getters.isMappingValid()") Create Mapping
+</template>
+
+<script>
+export default {
+  name: 'Header'
+}
+</script>
+
+<style scoped lang="scss">
+  .is-clearfix {
+    min-width: 25rem;
+    max-width: 25rem;
+    padding: 1rem 1rem;
+    z-index: 10;
+  }
+
+  a {
+    margin-left: 0.5rem;
+  }
+</style>
